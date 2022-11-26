99a328e3ff5de59cdc55760528ac03cfb00c5e5b
Rohith Sriramula <116960651+sriramulaRohith@users.noreply.github.com>
2022-11-26 11:43:11 +0530

    Add files via upload

---
+++ sumofnumbers.js
@@ -0,0 +1,16 @@
+/*let sum=0;
+let n = parseInt(prompt("enter the integer"));
+sum = n*(n+1)/2;
+console.log("the sum of numbers is",sum);*/
+
+
+
+let text="";
+let i =0;
+do{
+    text += 'the number is'+i;
+    ++i;
+}
+while(i<=10);
+//document.getElementById("mytext").innerHTML = text;
+console.log(text);
